0315b9d3a94df6dc0386f481f5b1c2c228c3eda0
Guillaume Dorce <guillaume.dorce@epita.fr>
2020-04-02 23:26:25 +0200

    recover logs from server with pastebin

---
+++ commands/log.js
@@ -0,0 +1,20 @@
+const index = require("../index.js");
+const { pastebinKey } = require("../config/config.json");
+const PastebinAPI = require("pastebin-js");
+let pastebin = new PastebinAPI(pastebinKey);
+
+module.exports = {
+    name: "log",
+    description: "Get all logs from the server.",
+    cooldown: 5,
+    execute(message, args) {
+        index.readDatabase("guild/" + message.guild.id).then(data => {
+            pastebin
+                .createPaste(JSON.stringify(data.val()))
+                .then(paste => {
+                    message.channel.send(paste);
+                })
+                .fail(err => console.log(err));
+        });
+    }
+};
